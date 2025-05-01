ca7bed2e7614f18c30071ebfd7548aaa1116a2ad
Stefania Gutierrez <gutierrezstefania02@gmail.com>
2025-05-01 16:21:22 -0500

    NorthWind.core y NorthWind.IoT - Sesion 4 - Segunda parte

---
+++ NorthWind/NorthWind.core/Services/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.ValueObjects;
+
+namespace NorthWind.ConsoleApp.Services;
+
+internal class ProductService(IUserActionWriter writer): IProductService
+{
+    public void Add(string user, string productName)
+    {
+        UserAction Action = new UserAction(user, $"Created: {productName}");
+        writer.Write(Action);
+    }
+}
+
