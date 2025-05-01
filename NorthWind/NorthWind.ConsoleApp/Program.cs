780e97ee750113eec774edf1ef26004ff0885992
Stefania Gutierrez <gutierrezstefania02@gmail.com>
2025-05-01 16:18:18 -0500

    NorthWind.ConsoleApp y NorthWind.Writers - Sesion 4

---
+++ NorthWind/NorthWind.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿HostApplicationBuilder Builder = Host.CreateApplicationBuilder();
+
+Builder.Services.AddNorthWindServices();
+
+using IHost AppHost = Builder.Build();
+
+IAppLogger Logger = AppHost.Services.GetRequiredService<IAppLogger>();
+Logger.WriteLog("Application started.");
+
+IProductService Service = AppHost.Services.GetRequiredService<IProductService>();
+Service.Add("Demo", "Azúcar refinada");
+
+/*
+ * AppLogger y los Writers: Responsabilidad única
+ * AppLogger: Abierto pero cerrado
+ * AppLogger: Inversión de dependencias. Los módulos
+ * de alto nivel son independientes de los detalles de implementación.
+ */
---
+++ NorthWind/NorthWind.Writers/ConsoleWriters.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthWind.Writers;
+
+internal class ConsoleWriter : IUserActionWriter
+{
+    public void Write(UserAction action)
+    {
+        Console.WriteLine("ConsoleWriter: {0}, {1}, {2}",
+            action.CreatedDateTime, action.User, action.Description);
+    }
+}
